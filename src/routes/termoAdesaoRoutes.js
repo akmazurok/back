53136c83832319818330e400acfc86aca5f01969
akmazurok <ak.mazurok@gmail.com>
2023-06-18 16:19:10 -0300

    Visualizar, aceitar e rescindir termo ok

--- src/routes/termoAdesaoRoutes.js
+++ src/routes/termoAdesaoRoutes.js
@@ -5,6 +5,8 @@ const termoAdesaoController = require("../controllers/termoAdesaoController");
 
 router.post("/:idUserEnt/:inscricaoid", termoAdesaoController.gerarTermo);
 router.get("/:idUserEst/:termoid", termoAdesaoController.visualizarTermo);
+router.patch("/:idUserEst/:termoid/aceitar", termoAdesaoController.aceitarTermo);
+router.patch("/:idUserEst/:termoid/rescindir", termoAdesaoController.rescindirTermo);
 
 
 module.exports = router;
